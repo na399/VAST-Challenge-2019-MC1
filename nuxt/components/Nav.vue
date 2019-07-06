b39a3718165bcc9f88b7be9a09a4c7e052b55012
Max Adulyanukosol <namax7@gmail.com>
2019-07-07 01:47:39 +0700

    Add Navbar

---
+++ nuxt/components/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nav">
+    <el-menu :collapse="true" :router="true">
+      <el-menu-item index="/">
+        <i class="el-icon-house"></i>
+        <span slot="title">Home</span>
+      </el-menu-item>
+      <el-submenu index="/">
+        <template slot="title">
+          <i class="el-icon-data-analysis"></i>
+          <span slot="title">Pages</span>
+        </template>
+        <el-menu-item index="MC1-demo">Demo Dashboard</el-menu-item>
+        <el-menu-item index="MC1">Full Dashboard</el-menu-item>
+        <el-menu-item index="Heatmap-multi">Multiple Heat Map</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="about">
+        <i class="el-icon-user"></i>
+        <span slot="title">About Us</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+import { Menu } from 'element-ui'
+
+export default {}
+</script>
+
+<style>
+.nav {
+  opacity: 0.5;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+}
+</style>
--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <Nav />
     <nuxt />
   </div>
 </template>
+
+<script>
+import Nav from '~/components/Nav.vue'
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
 
 <style>
 html {
